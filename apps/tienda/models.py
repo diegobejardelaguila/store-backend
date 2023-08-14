fc9b06dada6754677229340eab03994a44bf1648
Diego Bejar <diebejardelaguila@gmail.com>
2023-08-14 14:02:13 -0500

    first commit

--- apps/tienda/models.py
+++ apps/tienda/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-from django.conf import settings
-User = settings.AUTH_USER_MODEL
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 # Create your models here.
 
